05dd36c46e6f0484b388f3854461344ca7f331c6
Gloman21 <Gloman21@github.com>
2024-08-04 13:05:38 -0700

    task done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python3
+"""
+N queens
+"""
+
+import sys
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+    try:
+        a_q = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if a_q < 4:
+        print('N must be at least 4')
+        exit(1)
+
+
+    def solve_nqueens(n):
+        '''Self descriptive'''
+        if n == 0:
+            return [[]]
+        inner_solution = solve_nqueens(n - 1)
+        return [solution + [(n, i + 1)]
+                for i in range (a_q)
+                for solution in inner_solution
+                if safe_queen((n, i + 1), solution)]
+
+
+    def attack_queen(square, quen):
+        '''Self descriptive'''
+        (row1, col1) = square
+        (row2, col2) = queen
+        return (row1 == row2) or (col1 == col2) or\
+                abs(row1 - row2) == abs(col1 - col2)
+
+
+
+    def safe_queen(sqr, queens):
+        '''Self descriptive'''
+        for queen in queens:
+            if attack_queen(sqr, queen):
+                return False
+            return True
+
+
+
+    for answer in reversed(solved_nqueens(a_q)):
+        result = []
+        for p in [list(p) for p in answer]:
+            result.append([i -1 for i in p])
+        print(result)
+
